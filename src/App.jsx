38390bc3b262d3bbf96dec8103f481f68b2d3353
sanjarDev1 <sanjarbekqobulov02@gmail.com>
2021-12-18 23:56:59 +0500

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
+import React from "react";
 import "./App.css";
-import logo from "./images/ocin-removebg-preview.png"
-import logo2 from "./images/download-removebg-preview.png"
+import logo from "./images/ocin-removebg-preview.png";
+import logo2 from "./images/download-removebg-preview.png";
 export default class App extends Component {
-
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props); 
     this.state = {
       active: false,
-      href: "#"
-    }
-
+      href: "#",
+    };
   }
   render() {
-    const clic = ()=>{
+    const clic = () => {
       this.setState({
-        active: !this.state.active 
-      })
-    }
+        active: !this.state.active,
+      });
+    };
     return (
-      <div className='wrapper'>
-        <div className='card'>
-{
-  this.state.active? <img src={logo} alt="" />: 
-          <img src={logo2} className='logo2' alt="" />
-}      
-        <h1>{this.state.active ? "Create account" : "Welcome!"}</h1>
-        <input type="text" placeholder='Name' />
-        {
-          this.state.active && <input type="email" placeholder='Email' />
-        }
-        <input type="text" placeholder='Password' />
-        <button>{this.state.active ? "Create" : "Login" }</button>
-        <a href={this.state.href} onClick={()=>clic()}>{this.state.active ? "Sign In" : "Create account"}</a>
+      <div className="wrapper">
+        <div className="card">
+          {this.state.active ? (
+            <img src={logo} alt="" />
+          ) : (
+            <img src={logo2} className="logo2" alt="" />
+          )}
+          <h1>{this.state.active ? "Create account" : "Welcome!"}</h1>
+          <input type="text" placeholder="Name" />
+          {this.state.active && <input type="email" placeholder="Email" />}
+          <input type="text" placeholder="Password" />
+          <button>{this.state.active ? "Create" : "Login"}</button>
+          <a href={this.state.href}  onClick={() => clic()}>
+            {this.state.active ? "Sign In" : "Create account"}
+          </a>
+          <input>
         </div>
       </div>
-    )
+    );
   }
 }
